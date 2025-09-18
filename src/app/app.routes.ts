5661928d86fc93c2dbcc9b99424969f6a50505dd
Jesus Emilio Osorio Perez <jeosoriop@ufpso.edu.co>
2025-09-18 14:24:18 -0500

    login con register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    loadComponent: () =>
+    import('./view/main-page/main-page').then(
+        c => c.MainPage
+    )
+  },
+  {
+    path:'register',
+    loadComponent: ()=>
+      import('./view/register/register').then(
+        c=>c.Register
+      )
+  },
+  {
+    path:'login',
+    loadComponent:() =>
+      import('./pages/login/login').then(
+        c=>c.Login
+      )
+  },
+  {
+    path:'header',
+    loadComponent:()=>
+      import('./view/header/header').then(
+        c=>c.Header
+      )
+  },
+
+
+];
