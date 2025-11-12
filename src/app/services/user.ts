aabb1fbec727bc53f14d685bd92125e4051ea166
navegador3 <elnavegador.389@gmail.com>
2025-11-12 16:28:32 -0500

    arreglo de cors

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,13 +1,15 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class User {
   private httpClient = inject(HttpClient);
-  private baseUrl = 'https://emprenderedbackend-production.up.railway.app/usuarios2';
+  // private baseUrl = 'https://emprenderedbackend-production.up.railway.app/usuarios2';
+  private baseUrl = `${environment.apiUrl}/usuarios2`;
 
   register(userData: any): Observable<any> {
     return this.httpClient.post(this.baseUrl, userData);
